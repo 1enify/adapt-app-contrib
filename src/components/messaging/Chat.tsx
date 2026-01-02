fdbe677ef105ee506bb0037898bd056b4df2d448
jay3332 <40323796+jay3332@users.noreply.github.com>
2026-01-01 19:13:42 -0500

    remove "loading messages"

--- src/components/messaging/Chat.tsx
+++ src/components/messaging/Chat.tsx
@@ -2204,13 +2204,6 @@ export default function Chat(props: {
           </span>
         </Show>
       </div>
-
-      <Show when={isFetching()}>
-        <div class="fixed bottom-24 right-4 bg-bg-2 text-fg px-4 py-2 rounded-full shadow-lg flex items-center gap-2">
-          <Icon icon={Spinner} class="w-4 h-4 fill-fg sanimate-spin" />
-          Loading messages...
-        </div>
-      </Show>
     </div>
   );
 }
